79a88678d5cbf87c86dcceb3b772c62854ebb8ed
yule-go-dev <dev@yule-go.com>
2026-05-07 22:52:22 +0800

    fix: vite dev server 监听 0.0.0.0 支持外部访问

--- src/miniprogram/vite.config.js
+++ src/miniprogram/vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 
 export default defineConfig({
-  plugins: [uni()]
+  plugins: [uni()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  }
 })
